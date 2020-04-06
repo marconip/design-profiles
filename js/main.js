91cfd3134bd5b84d98caabf3521d5aa3f552685c
marconip <40406938+marconip@users.noreply.github.com>
2020-04-06 11:28:39 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ var resultado = document.querySelector(".btn-resultado");
 var comecar = document.querySelector(".btn-nao-deisgn");
 var inicio = document.querySelector(".inicio");;
 var asPerguntas = document.querySelectorAll("section");
-var numero = 0;
 var numPergunta = document.querySelector(".numeros-perguntas");
+var numero = 0;
+
 
 /*
 //quando iniciar direto com perguntas
@@ -75,4 +76,66 @@ voltar.addEventListener("click", function () {
         resultado.style.display = "none"
         passar.style.display = "inline-block";
     }
+});
+
+
+//Pegar click das respostas
+var design_produto = "0";
+var ux_design = "0";
+var design_grafico = "0";
+var game_design = "0";
+var design_digital = "0";
+
+function diminuir(...respostas) {
+    respostas.forEach(function (respostas) {
+        if (resposta > 0) {
+            resposta--;
+        }
+    })
+}
+
+var dataPerfils = document.querySelectorAll(".pergunta1 li");
+
+Array.prototype.forEach.call(dataPerfils, function (dataPerfil) {
+    var estePerfil = dataPerfil.getAttribute("data-profissao");
+
+    dataPerfil.addEventListener("click", function (e) {
+
+        design_produto = diminuir(design_produto); design_grafico--; design_digital--;
+
+        console.clear();
+        if (dataPerfils.getAttribute("class") == true) {
+            dataPerfil.removeAttribute("class");
+        }
+        setTimeout(function () {
+            e.classList.add("selecionado");
+        }, 150);
+        switch (estePerfil) {
+            case "1":
+                console.log("opção 1");
+                design_produto++;
+                design_grafico++;
+                design_digital++;
+                break;
+            case "2":
+                console.log("opção 2");
+                design_grafico++;
+                design_digital++;
+                break;
+            case "3":
+                console.log("opção 3");
+                design_digital++;
+                break;
+            case "4":
+                console.log("opção 4");
+                ux_design++;
+                break;
+            case "5":
+                console.log("opção 5");
+                game_design++;
+                break;
+            default:
+                alert("É nescessário escolher 1 opção!");
+        }
+    });
 });
