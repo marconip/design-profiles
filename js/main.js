1f1d3107c603bfe7cbfcdbdfe8d583fc22070631
marconip <marconidesenhos@gmail.com>
2020-04-11 14:04:16 -0300

    novo modelo

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 var body = document.querySelector("body");
-var voltar = document.querySelector(".btn-voltar");
 var passar = document.querySelector(".btn-proximo");
 var resultado = document.querySelector(".btn-resultado");
+var reiniciar = document.querySelector(".btn-reiniciar");
 var comecar = document.querySelector(".btn-nao-deisgn");
 var inicio = document.querySelector(".inicio");;
 var asPerguntas = document.querySelectorAll("section");
 var numPergunta = document.querySelector(".numeros-perguntas");
 var numero = 0;
+var design_marca = 0;
+var web_design = 0;
+var design_grafico = 0;
+var design_produto = 0;
+var ux_design = 0;
+var ui_design = 0;
+var design_digital = 0;
+var design_personagem = 0;
+var design_jogos = 0;
+var design_editorial = 0;
 
-
-/*
-//quando iniciar direto com perguntas
-    window.onload = function () {
-    mostrarPerguntas();
-}; */
-
+//Click de escolha para começar
 comecar.onclick = function () {
     inicio.classList.add("anime-desaparecer");
     setTimeout(function () {
@@ -34,6 +38,8 @@ function mostrarPerguntas() {
         }, 800);
         if (numero == i) {
             setTimeout(function () {
+                passar.setAttribute("disabled", "disabled");
+                resultado.setAttribute("disabled", "disabled");
                 asPergunta.style.display = "block";
                 asPergunta.classList.add("anime-aparecer");
             }, 800);
@@ -42,60 +48,17 @@ function mostrarPerguntas() {
     })
 };
 
-//quando clicar no teclados de setas
-body.onkeyup = function (e) {
-    if (e.code == "ArrowRight") {
-        passar.click();
-    } else if (e.code == "ArrowLeft") {
-        /* voltar.click(); */
-    }
-};
-
 //click no botão passar
 passar.addEventListener("click", function () {
-    if (document.getElementsByClassName('anime-aparecer').item(0).getElementsByClassName('selecionado').length === 0) {
-        alert("É nescessário escolher 1 opção!");
-        return;
-    }
-
     numero++;
     if (numero > asPerguntas.length - 1)
         numero = asPerguntas.length - 1;
-    mostrarPerguntas()
-    /* voltar.style.display = "inline-block"; */
+    mostrarPerguntas();
     if (numero == asPerguntas.length - 1) {
         passar.style.display = "none";
         resultado.style.display = "inline-block"
     }
 });
-/* 
-//click no botão voltar
-voltar.addEventListener("click", function () {
-    numero--;
-    if (numero < 0)
-        numero = 0;
-    mostrarPerguntas();
-    if (numero == 0) {
-        voltar.style.display = "none";
-    } else if (numero < asPerguntas.length - 1) {
-        resultado.style.display = "none"
-        passar.style.display = "inline-block";
-    }
-}); */
-
-//Pegar click das respostas
-var design_marca = 0;
-var web_design = 0;
-var design_grafico = 0;
-var design_produto = 0;
-var ux_design = 0;
-var ui_design = 0;
-var design_digital = 0;
-var design_personagem = 0;
-var design_jogos = 0;
-var design_editorial = 0;
-
-
 
 resultado.onclick = function () {
     var array = [
@@ -117,30 +80,45 @@ resultado.onclick = function () {
     var perfil = document.querySelectorAll("article");
     perfil.forEach(function (el) {
         if (el.className === resultadoPerfil) {
-            el.style.display = "block";
+            document.querySelector(".anime-aparecer").classList.add("anime-desaparecer");
+            setTimeout(function () {
+                document.querySelector(".anime-aparecer").style.display = "none";
+            }, 800);
+            setTimeout(function () {
+                el.classList.add("anime-aparecer");
+                el.style.display = "block";
+            }, 800);
+            numPergunta.parentElement.style.display = "none";
+            resultado.style.display = "none";
+            reiniciar.style.display = "inline-block"
         }
     });
-}
+};
 
-/* function decrementar(resposta) {
+function decrementar(resposta) {
     if (resposta > 0) return resposta -= 1;
     return resposta;
-} */
+}
 
 var dataPerfilsList = document.querySelectorAll("section li");
 var dataPerfils = Array.from(dataPerfilsList);
 
 dataPerfils.forEach(dataPerfil => {
-    var estePerfil = dataPerfil.getAttribute("data-profissao");
 
     dataPerfil.addEventListener("click", function (e) {
+        passar.removeAttribute("disabled");
+        resultado.removeAttribute("disabled");
 
-        /* web_design = decrementar(web_design);
+        design_marca = decrementar(design_marca);
+        web_design = decrementar(web_design);
         design_grafico = decrementar(design_grafico);
+        design_produto = decrementar(design_produto);
+        ux_design = decrementar(ux_design);
         ui_design = decrementar(ui_design);
         design_digital = decrementar(design_digital);
-        design_editorial = decrementar(design_editorial); 
-        console.clear();*/
+        design_personagem = decrementar(design_personagem);
+        design_jogos = decrementar(design_jogos);
+        design_editorial = decrementar(design_editorial);
 
         //Mostra a resposta selecionado
         var perfilJaSelecionado = dataPerfils.filter(p => p.classList.contains("selecionado"));
@@ -149,6 +127,7 @@ dataPerfils.forEach(dataPerfil => {
         }
         setTimeout(() => this.classList.add("selecionado"), 150);
 
+        var estePerfil = dataPerfil.getAttribute("data-respostaNumero");
         switch (estePerfil) {
             case "1":
                 console.log("web_design, ui_design");
@@ -173,6 +152,7 @@ dataPerfils.forEach(dataPerfil => {
                 design_editorial++;
                 break;
 
+            //pergunta 2
             case "6":
                 console.log("web_design, ui_design");
                 web_design++;
@@ -196,6 +176,7 @@ dataPerfils.forEach(dataPerfil => {
                 design_editorial++;
                 break;
 
+            //pergunta 3
             case "11":
                 console.log("web_design, ui_design");
                 web_design++;
